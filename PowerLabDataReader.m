dc19f186e4fe96bfdba8879c7060ce9030f268b2
Rohan Vora <rohan.c.vora@gmail.com>
2021-10-15 11:33:52 -0400

    Friday date added

--- PowerLabDataReader.m
+++ PowerLabDataReader.m
@@ -1,5 +1,6 @@
 function [TIME, RED, NIR] = ...
 % this is a function used for reading data 
+% hi today is friday
 PowerLabDataReader(file_name, format,...
     RED_offset, NIR_offset, FS_raw, TS)
 
